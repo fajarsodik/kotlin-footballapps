e7c69ad00d4df2c030fb12c93d2d7180a0007796
Mochamad Fajar Sodik <mochamad.fajarsodik@gmail.com>
2018-10-16 07:42:27 +0700

    init commit

---
+++ app/src/main/java/com/hevadevelop/footballclub/RecyclerViewAdapter.kt
@@ -0,0 +1,35 @@
+package com.hevadevelop.footballclub
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.squareup.picasso.Picasso
+
+class RecyclerViewAdapter(private val context: Context, private val items: List<Item>)
+    : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder (LayoutInflater.from(context).inflate(R.layout.item_list, parent, false))
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindItem(items[position])
+    }
+
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        private val nama = view.findViewById<TextView>(R.id.nama_klub)
+        private val image = view.findViewById<ImageView>(R.id.image_club)
+
+        fun bindItem(items: Item) {
+            nama.text = items.name
+            items.image?.let { Picasso.get().load(it).into(image) }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/hevadevelop/footballclub/item.kt
@@ -0,0 +1,3 @@
+package com.hevadevelop.footballclub
+
+data class Item (val name: String?, val image: Int?)

46b24f0db8d6085f2cda9ff565440246608f5d28
Mochamad Fajar Sodik <mochamad.fajarsodik@gmail.com>
2018-11-28 07:15:51 +0700

    fix kotlin version

--- app/src/main/java/com/hevadevelop/footballapps/presenter/MainPresenter.kt
+++ app/src/main/java/com/hevadevelop/footballapps/presenter/MainPresenter.kt
@@ -5,6 +5,9 @@ import com.hevadevelop.footballapps.api.ApiRepository
 import com.hevadevelop.footballapps.api.TheSportDb
 import com.hevadevelop.footballapps.model.EventsResponse
 import com.hevadevelop.footballapps.view.MainView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
@@ -13,16 +16,14 @@ class MainPresenter(private val view: MainView,
                     private val gson: Gson) {
     fun getCompetitionList(typeList: String?, param: String?, valueParam: String?) {
         view.showLoading()
-        doAsync {
-            val data = gson.fromJson(apiRepository
-                    .doRequest(TheSportDb.getMatch(typeList, param, valueParam)),
-                    EventsResponse::class.java
-            )
 
-            uiThread {
-                view.hideLoading()
-                view.showEventList(data.events)
-            }
+        GlobalScope.launch(Dispatchers.Main) {
+            val data = gson.fromJson(apiRepository
+                    .doRequest(TheSportDb.getMatch(typeList, param, valueParam))
+                    .await(),
+                    EventsResponse::class.java)
+            view.showEventList(data.events)
+            view.hideLoading()
         }
     }
 }
--- app/src/main/java/com/hevadevelop/footballapps/presenter/TeamPresenter.kt
+++ app/src/main/java/com/hevadevelop/footballapps/presenter/TeamPresenter.kt
@@ -6,6 +6,9 @@ import com.hevadevelop.footballapps.api.TheSportDb
 import com.hevadevelop.footballapps.model.EventsResponse
 import com.hevadevelop.footballapps.model.TeamsResponse
 import com.hevadevelop.footballapps.view.DetailMatchView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
@@ -15,46 +18,72 @@ class TeamPresenter(private val view: DetailMatchView,
 
     fun getMatchList(typeList: String?, param: String?, valueParam: String?) {
         view.showLoading()
-        doAsync {
-            val data = gson.fromJson(apiRepository
-                    .doRequest(TheSportDb.getMatch(typeList, param, valueParam)),
-                    EventsResponse::class.java
-            )
 
-            uiThread {
-                view.hideLoading()
-                view.showDetailMatch(data.events)
-            }
+        GlobalScope.launch(Dispatchers.Main) {
+            val data = gson.fromJson(apiRepository
+                    .doRequest(TheSportDb.getMatch(typeList, param, valueParam))
+                    .await(),
+                    EventsResponse::class.java)
+            view.showDetailMatch(data.events)
+            view.hideLoading()
         }
+//        doAsync {
+//            val data = gson.fromJson(apiRepository
+//                    .doRequest(TheSportDb.getMatch(typeList, param, valueParam)),
+//                    EventsResponse::class.java
+//            )
+//
+//            uiThread {
+//                view.hideLoading()
+//                view.showDetailMatch(data.events)
+//            }
+//        }
     }
 
     fun getHomeDetail(typeList: String?, param: String?, valueParam: String?) {
         view.showLoading()
-        doAsync {
+        GlobalScope.launch(Dispatchers.Main) {
             val data = gson.fromJson(apiRepository
-                    .doRequest(TheSportDb.getMatch(typeList, param, valueParam)),
-                    TeamsResponse::class.java
-            )
-
-            uiThread {
-                view.hideLoading()
-                view.showTeamListHome(data.teams)
-            }
+                    .doRequest(TheSportDb.getMatch(typeList, param, valueParam))
+                    .await(),
+                    TeamsResponse::class.java)
+            view.showTeamListHome(data.teams)
+            view.hideLoading()
         }
+//        doAsync {
+//            val data = gson.fromJson(apiRepository
+//                    .doRequest(TheSportDb.getMatch(typeList, param, valueParam)),
+//                    TeamsResponse::class.java
+//            )
+//
+//            uiThread {
+//                view.hideLoading()
+//                view.showTeamListHome(data.teams)
+//            }
+//        }
     }
 
     fun getAwayDetail(typeList: String?, param: String?, valueParam: String?) {
         view.showLoading()
-        doAsync {
-            val data = gson.fromJson(apiRepository
-                    .doRequest(TheSportDb.getMatch(typeList, param, valueParam)),
-                    TeamsResponse::class.java
-            )
 
-            uiThread {
-                view.hideLoading()
-                view.showTeamListAway(data.teams)
-            }
+        GlobalScope.launch(Dispatchers.Main) {
+            val data = gson.fromJson(apiRepository
+                    .doRequest(TheSportDb.getMatch(typeList, param, valueParam))
+                    .await(),
+                    TeamsResponse::class.java)
+            view.showTeamListAway(data.teams)
+            view.hideLoading()
         }
+//        doAsync {
+//            val data = gson.fromJson(apiRepository
+//                    .doRequest(TheSportDb.getMatch(typeList, param, valueParam)),
+//                    TeamsResponse::class.java
+//            )
+//
+//            uiThread {
+//                view.hideLoading()
+//                view.showTeamListAway(data.teams)
+//            }
+//        }
     }
 }
